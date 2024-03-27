c0e3188a8fd3237fab371b6ec67b4b6c91cea638
wellitsabhi <abhishekongit@gmail.com>
2024-03-27 18:54:43 +0530

    Footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
@@ -16,6 +17,7 @@ export default function Home() {
       <MusicSchoolTestimonials/>
       <UpcomingWebinars/>
       <Instructors />
+      <Footer />
    </main>
   );
 }
